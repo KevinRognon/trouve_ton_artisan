decb71e2abd244ee70ae9053cb788c658ac94340
Kevin Rognon <rognonk@gmail.com>
2023-07-22 15:09:11 +0200

    Mise à jour

--- src/Components/Header/Header-Modal.jsx
+++ src/Components/Header/Header-Modal.jsx
@@ -40,14 +40,14 @@ export default function HeaderModal ({showModal, setModalState}) {
 
 
     return (
-        <div className="conteneur">
+        <div className="overflow-x-hidden">
             <div className="overlay" onClick={closeModal}>
                 <div className="searchbar_modal">
                     <form onSubmit={HandleSubmit}>
                         <h1>Recherchez votre artisan</h1>
                         <input ref={recherche} onClick={HandleInput} placeholder="Recherchez un artisan" type="text"/>
                         <button type="submit" className="btn-rechercher btn">Rechercher</button>
-                        <button onClick={closeModal} className="btn-fermer btn btn-danger">Fermer</button>
+                        {/*<button onClick={closeModal} className="btn-fermer btn btn-danger">Fermer</button>*/}
                     </form>
                 </div>
             </div>
