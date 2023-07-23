71fd017a0bef035d73dcf0f85968291ecd64f5e3
Kevin Rognon <rognonk@gmail.com>
2023-07-23 16:09:22 +0200

    Mise à jour

--- src/Pages/Accueil/Accueil.jsx
+++ src/Pages/Accueil/Accueil.jsx
@@ -3,23 +3,36 @@ import data from '../../assets/Data/datas.json';
 import EntrepriseCard from "../../Components/EntrepriseCard/EntrepriseCard";
 import {useEffect, useState} from "react";
 
+import plombier from "./assets/plombier.png";
+import couture from "./assets/couture.png";
+import coiffure from "./assets/coiffure.png";
+import bijoutier from "./assets/bijou.png";
+import charpentier from "./assets/marteau.png";
+import macon from "./assets/marteau.png";
+
+import './Accueil.scss';
+
 export default function () {
 
 
+
     let tableauEntreprises = useState([]);
-    let troisEntreprises   = [];
+    let [troisEntreprises, setEntreprise]   = useState([]);
 
     useEffect(() => {
         tableauEntreprises = [];
-        troisEntreprises   = [];
+        const troisEntreprisesTemp   = [];
 
         data.map((item) => {
             tableauEntreprises.push(item);
         })
+        tableauEntreprises.sort((a, b) => b.note - a.note);
         for (let i = 0; i <= 2; i++) {
-            troisEntreprises.push(tableauEntreprises[i]);
+            troisEntreprisesTemp.push(tableauEntreprises[i]);
         }
-    })
+
+        setEntreprise(troisEntreprisesTemp);
+    }, [])
 
 
     return (
@@ -42,12 +55,21 @@ export default function () {
                     <Etape number="4" indication="Une réponse sera apportée sous 48h."/>
                 </div>
             </section>
-            <section>
-                <h1>Entreprises du mois</h1>
+            <section className="section-entreprises pt-4 pb-4">
+                <h1 className="text-light text-center">Entreprises du mois</h1>
                 <article className="d-flex flex-column justify-content-center align-items-center gap-2">
-                    {troisEntreprises.map((item) => {
-                        return <EntrepriseCard entreprise_nom={item.name} specialite={item.specialty} localisation={item.location} note={item.note}/>
-                    })}
+                    {
+                        troisEntreprises.map((item, key) => {
+                            switch (item.specialty) {
+                                case "Plombier":
+                                    return <EntrepriseCard key={item.id} icone_specialite={plombier} entreprise_nom={item.name} specialite={item.specialty} localisation={item.location} note={item.note} />
+                                case "Coiffeur":
+                                    return <EntrepriseCard key={item.id} icone_specialite={coiffure} entreprise_nom={item.name} specialite={item.specialty} localisation={item.location} note={item.note} />
+
+                            }
+                             })
+
+                    }
                 </article>
             </section>
         </>
