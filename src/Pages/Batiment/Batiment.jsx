e1cef65a3830a65f058b91c545c6bf57cc98187f
Kevin Rognon <rognonk@gmail.com>
2023-07-24 23:18:29 +0200

    Mise à jour

--- src/Pages/Batiment/Batiment.jsx
+++ src/Pages/Batiment/Batiment.jsx
@@ -4,21 +4,37 @@ import data from '../../assets/Data/datas.json';
 
 import './Batiment.scss';
 import EntrepriseCard from "../../Components/EntrepriseCard/EntrepriseCard";
+import {useState} from "react";
 
 export default function Batiment () {
+
+    const [inputResearch, setInputResearch] = useState("");
+
+    const handleInputChange = (e) => {
+        let valeur = e.target.value;
+        valeur.length > 2 && setInputResearch(valeur);
+        valeur.length === 0 && setInputResearch("");
+    }
 
     return (
         <section className="d-flex flex-column align-items-center">
             <article className="champ_recherche d-flex flex-row justify-content-center align-items-center card col-8 rounded-4 p-1 gap-1 mb-4 mt-3">
                 <Loupe />
-                <input type="text" placeholder="Recherchez un artisan"/>
+                <input onChange={handleInputChange} type="text" placeholder="Recherchez un artisan"/>
             </article>
 
             <article className="article-batiment w-100 d-flex flex-column justify-content-center align-items-center">
                 <div className="d-flex flex-column justify-content-center align-items-center col-12 gap-2 p-2 mt-2 mb-2">
+                    <h1 className="text-light">Entreprises du bâtiment</h1>
                     {
-                        data.map((item, key) =>
-                            <EntrepriseCard key={item.id} icone_specialite={item.icone} entreprise_nom={item.name} specialite={item.specialty} localisation={item.location} note={item.note} />
+                        data.filter((val) => {
+                            return val.specialty.toLowerCase().includes(inputResearch.toLowerCase());
+                        }).map((val, key) => {
+                                if (val.category === "Bâtiment") {
+                                    return <EntrepriseCard key={val.id} icone_specialite={val.icone} entreprise_nom={val.name} specialite={val.specialty} localisation={val.location} note={val.note} />
+                                }
+                                return null;
+                            }
                         )
                     }
                 </div>
