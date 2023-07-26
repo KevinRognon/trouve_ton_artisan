0b8a88d80d28bc017ac0d36e1ea8c27383b3a4a3
Kevin Rognon <rognonk@gmail.com>
2023-07-26 12:03:32 +0200

    Mise à jour

--- src/Pages/Alimentation/Alimentation.jsx
+++ src/Pages/Alimentation/Alimentation.jsx
@@ -18,14 +18,16 @@ export default function Alimentation () {
 
     return (
         <section className="d-flex flex-column align-items-center">
-            <article className="champ_recherche d-flex flex-row justify-content-center align-items-center card col-8 rounded-4 p-1 gap-1 mb-4 mt-3">
-                <Loupe />
-                <input onChange={handleInputChange} type="text" placeholder="Recherchez un artisan"/>
+            <article className="d-flex flex-column justify-content-center align-items-center">
+                <h1 className="text-dark">Entreprises d'Alimentation</h1>
+                <div className="champ_recherche d-flex flex-row justify-content-center align-items-center card col-8 rounded-4 p-1 gap-1 mb-4 mt-3">
+                    <Loupe />
+                    <input onChange={handleInputChange} type="text" placeholder="Recherchez un artisan"/>
+                </div>
             </article>
 
             <article className="article-batiment w-100 d-flex flex-column justify-content-center align-items-center">
                 <div className="d-flex flex-column justify-content-center align-items-center col-12 gap-2 p-2 mt-2 mb-2">
-                    <h1 className="text-light">Entreprises d'Alimentation</h1>
                     {
                         data.filter((val) => {
                             return val.specialty.toLowerCase().includes(inputResearch.toLowerCase()) || val.location.toLowerCase().includes(inputResearch.toLowerCase()) || val.name.toLowerCase().includes(inputResearch.toLowerCase());
