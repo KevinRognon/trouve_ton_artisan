080f161d5cc6a25b003aaf67a11ead4edcde65b0
Kevin Rognon <rognonk@gmail.com>
2023-07-29 21:51:25 +0200

    Mise à jour de la page détail

--- src/Components/EntrepriseDetail/EntrepriseDetail.jsx
+++ src/Components/EntrepriseDetail/EntrepriseDetail.jsx
@@ -7,21 +7,26 @@ export default function EntrepriseDetail ( props ) {
 
 
     return (
-        <div>
-            <div>
+        <div className="d-flex flex-column justify-content-center p-4 gap-3">
+            <div className="align-self-center">
                 <MapPosition fill="crimson" />
                 {props.localisation}
             </div>
             <div>
-                <h1>{props.specialite}</h1>
-                <div className="d-flex justify-content-center align-items-center">
-                    <img src={props.icone} alt={props.entreprise_nom}/>
+                <h1 className="text-decoration-underline">{props.specialite}</h1>
+                <div className="d-flex align-items-center gap-2">
                     <h2 className="fs-6">{props.entreprise_nom}</h2>
                     <div className="d-flex">
                         <p>{props.note}</p>
                         <Note fill="#FFD600" />
                     </div>
                 </div>
+            </div>
+            <div>
+                <h2 className="text-decoration-underline mb-2">A propos</h2>
+                <p className="mx-2">
+                    {props.apropos}
+                </p>
             </div>
         </div>
     )
--- src/Pages/EntrepriseDetailPage/EntrepriseDetailPage.jsx
+++ src/Pages/EntrepriseDetailPage/EntrepriseDetailPage.jsx
@@ -12,11 +12,11 @@ export default function EntrepriseDetailPage () {
 
 
     return (
-        <section className="section-information d-flex flex-column justify-content-start align-items-center gap-2">
+        <section className="section-information d-flex flex-column justify-content-start align-items-center">
             <article className="article-titre w-100 text-center mb-1">
                 <h1>Informations</h1>
             </article>
-            <article className="article-information col-11">
+            <article className="article-information col-11 mt-3">
                 {
                     data.map((entreprise, key) => {
                         if(params.name === entreprise.name) {
@@ -27,16 +27,24 @@ export default function EntrepriseDetailPage () {
                                 specialite={entreprise.specialty}
                                 localisation={entreprise.location}
                                 note={entreprise.note}
+                                apropos={entreprise.about}
                             />
                         }
                     })
                 }
             </article>
 
-            <article className="article-form">
-                <form>
-                    <h1>Formulaire de contact</h1>
-                </form>
+            <article className="article-form col-11 p-3 mb-3">
+                <div className="d-flex flex-column justify-content-center align-content-center col-12">
+                    <h1 className="fs-3 text-center">Formulaire de contact</h1>
+                    <form className="d-flex flex-column justify-content-center align-items-center gap-2">
+                        <input className="col-10 p-3 border-black border-1 rounded-1" type="text" placeholder="Nom"/>
+                        <input className="col-10 p-3 border-black border-1 rounded-1" type="text" placeholder="Objet"/>
+                        <input className="col-10 p-3 border-black border-1 rounded-1" type="text" placeholder="Message"/>
+
+                        <button className="btn btn-success">Envoyer</button>
+                    </form>
+                </div>
             </article>
         </section>
     )
