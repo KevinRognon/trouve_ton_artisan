c7ffe050239478078d20e1a05d764c4538a8a844
Kevin Rognon <rognonk@gmail.com>
2023-07-30 12:14:13 +0200

    Mise à jour

--- src/Pages/404/Error404.jsx
+++ src/Pages/404/Error404.jsx
@@ -1,8 +1,16 @@
 
+import error404 from '../../assets/icones/404.png';
+import {NavLink} from "react-router-dom";
 
 export default function Error404 () {
 
     return (
-        <h1>Erreur 404</h1>
+        <section className="d-flex flex-column justify-content-center align-items-center mt-5">
+            <img className="w-50 mb-4" src={error404} alt="Erreur 404"/>
+            <h1 className="text-center">La page recherchée est introuvable</h1>
+            <NavLink to="/" className="btn btn-primary pb-1 pt-1 rounded-4">
+                Retourner à l'accueil
+            </NavLink>
+        </section>
     )
 }
