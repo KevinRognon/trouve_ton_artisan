9a9ea541f7ce6df1d7dd3e0de1b9bf4c2a5e9408
Kevin Rognon <rognonk@gmail.com>
2023-07-30 15:32:29 +0200

    Mise à jour

--- src/Components/Etape/Etape.jsx
+++ src/Components/Etape/Etape.jsx
@@ -4,13 +4,13 @@ export default function Etape (props) {
 
 
     return (
-        <div className="d-flex flex-column justify-content-center align-items-center gap-3">
+        <div className="d-flex flex-column justify-content-center align-items-center gap-3 col-9">
 
             <span className="numero_etape bleu_primaire d-flex align-items-center justify-content-center text-light p-3 rounded-5">{props.number}</span>
 
             <span className="sep"></span>
 
-            <p className="text-center col-8">
+            <p className="text-center col-12">
                 {props.indication}
             </p>
         </div>
--- src/Pages/Accueil/Accueil.jsx
+++ src/Pages/Accueil/Accueil.jsx
@@ -40,25 +40,28 @@ export default function Accueil () {
         <>
             <section className="d-flex flex-column justify-content-center text-center mt-4 pb-5">
                 <h1 className="mb-5 col-12 p-2">Comment trouver mon artisan ?</h1>
-                <div className="d-flex justify-content-center">
-                    <Etape number="1" indication="Choisir une catégorie d'artisanat dans le menu."/>
-                </div>
 
-                <div className="d-flex justify-content-center">
-                    <Etape number="2" indication="Choisir un artisan."/>
-                </div>
+                <div className="d-flex flex-column flex-md-row justify-content-md-between align-items-center">
+                    <div className="d-flex justify-content-center">
+                        <Etape number="1" indication="Choisir une catégorie d'artisanat dans le menu."/>
+                    </div>
 
-                <div className="d-flex justify-content-center">
-                    <Etape number="3" indication="Le contacter via le formulaire."/>
-                </div>
+                    <div className="d-flex justify-content-center">
+                        <Etape number="2" indication="Choisir un artisan."/>
+                    </div>
 
-                <div className="d-flex justify-content-center">
-                    <Etape number="4" indication="Une réponse sera apportée sous 48h."/>
+                    <div className="d-flex justify-content-center">
+                        <Etape number="3" indication="Le contacter via le formulaire."/>
+                    </div>
+
+                    <div className="d-flex justify-content-center">
+                        <Etape number="4" indication="Une réponse sera apportée sous 48h."/>
+                    </div>
                 </div>
             </section>
             <section className="section-entreprises pt-4 pb-4 d-flex flex-column align-items-center">
                 <h1 className="text-light text-center p-3">Entreprises du mois</h1>
-                <article className="d-flex flex-column justify-content-center align-items-center gap-2 col-11">
+                <article className="d-flex flex-column flex-md-row justify-content-center align-items-center align-items-md-start gap-2 pb-md-5">
                     {
                         troisEntreprises.map((item, key) =>
                             <EntrepriseCard
